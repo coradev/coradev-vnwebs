06f06e81f6478d1d0992191dfd767cbfdc037c9c
Quang Truong ✔️ <truongnq2k@gmail.com>
2021-05-24 07:27:25 +0700

    coradev vnwebs

---
+++ src/main/java/com/coradev/vnwebs/VnwebsApplication.java
@@ -0,0 +1,13 @@
+package com.coradev.vnwebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnwebsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VnwebsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coradev/vnwebs/VnwebsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coradev.vnwebs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnwebsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
