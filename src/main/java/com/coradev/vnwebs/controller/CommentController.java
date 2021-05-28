54acc60c5d75ae0e4d11796bf759c38f1dd32c4b
Quang Truong ✔️ <truongnq2k@gmail.com>
2021-05-28 20:23:07 +0700

    update post page

--- src/main/java/com/coradev/vnwebs/controller/CommentController.java
+++ src/main/java/com/coradev/vnwebs/controller/CommentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 
-//@Controller
+@Controller
 public class CommentController {
 
     private final CommentService commentService;
